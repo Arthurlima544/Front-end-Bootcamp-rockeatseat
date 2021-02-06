c76c09279b27259cb332aa3fd8335393f93a007a
Arthur Lima <arthurlindaolima544@gmail.com>
2021-02-06 13:16:30 -0300

    working

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+    const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        compress: true,
+        port: 8000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    }
+}
